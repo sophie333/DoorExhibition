6fe189658883e8654e9ac4b514886dd4f1fc365b
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-10-23 17:53:09 +0200

    Set prototype up
    Architecture, Animation, Player Controller and Outside Lighting

---
+++ DoorExhibition/Assets/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+
+        if(player)
+        {
+            anim.Play("openDoor");
+        }
+    }
+}

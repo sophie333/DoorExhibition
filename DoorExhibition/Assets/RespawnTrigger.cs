920f72f05f1a55d9f32bfc03883888e1d12e0234
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-10-29 01:06:04 +0100

    added sound

---
+++ DoorExhibition/Assets/RespawnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class RespawnTrigger : MonoBehaviour {
+
+    [SerializeField]
+    private Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+
+        if (player)
+        {
+            player.GetComponent<Rigidbody>().isKinematic = true;
+            player.transform.position = new Vector3(2.7f, 2.3f, -2.7f);
+            anim.Play("closeDoor");
+        }
+    }
+}

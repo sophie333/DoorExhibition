df7b174a3ff4dc8802abed8a37788842ea1a5a75
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-06 15:34:51 +0100

    Added sounds, animation, textures, lighting and triggers
    door sounds: rattling, squeaking
    door animations: rattling, opening
    light entering door
    textures: door + individual parts, walls
    scripts: animation trigger

---
+++ DoorExhibition/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- DoorExhibition/Assets/Scripts/DoorTrigger.cs
+++ DoorExhibition/Assets/Scripts/DoorTrigger.cs
@@ -2,19 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using TMPro;
 
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField]
     private Animator anim;
 
-    private void OnTriggerEnter(Collider other)
+    [SerializeField]
+    private AudioSource s_rattlingDoor;
+
+    [SerializeField]
+    private AudioSource s_squeakingDoor;
+
+    [SerializeField]
+    private GameObject t_openDoorText;
+
+    private bool opened = false;
+    private bool triedOpen = false;
+
+    private void OnTriggerStay(Collider other)
     {
         FirstPersonController player = other.GetComponent<FirstPersonController>();
 
-        if(player)
+        if(player && !opened)
         {
-            anim.Play("openDoor");
+            t_openDoorText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E) && !opened)
+            {
+                s_rattlingDoor.Play();
+                anim.Play("a_doorRattle");
+                triedOpen = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+
+        if (player)
+        {
+            t_openDoorText.SetActive(false);
+
+            if (!opened && triedOpen)
+            {
+                opened = true;
+                s_squeakingDoor.Play();
+                anim.Play("a_doorOpen");
+            }
         }
     }
 }
